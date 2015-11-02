e92cb6210a112a348734ce1c9a122d69c9bb0b61
sangrampp <sangrampp@gmail.com>
2015-11-02 15:50:46 +0530

    Nothing works, restarting!

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,7 @@ gulp.task('serve', ['sass'], function() {
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function() {
-    return gulp.src("app/scss/*.scss")
+    return gulp.src("app/scss/styles.scss")
         .pipe(sass())
         .pipe(gulp.dest("app/css"))
         .pipe(browserSync.stream());
